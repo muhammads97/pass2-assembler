319c89807ad119c6430d074539a163dd6477b842
Muhammad Salah <msalah.29.10@gmail.com>
2019-05-13 00:08:52 +0200

    fixed infinite loop

--- src/Pass2.cpp
+++ src/Pass2.cpp
@@ -54,7 +54,7 @@ vector<Instruction> Pass2::execute() {
             j++;
             continue;
         }
-        if(opTab[j].getLabel() == string("*")){
+        if(opTab[j].getLabel() == string("*")) {
             int objectCode = getLiteralValue(opTab[j].getOperation());
             opTab[j].setOpCode(getHex(opTab[j].getStringOpCode()));
             cout << opTab[j].toString() <<endl;
@@ -77,31 +77,32 @@ vector<Instruction> Pass2::execute() {
         if(op[0] == '+') {
             e = true;
         }
-        if(regex_match(op, skipOperations)){
-            if(op == string("byte")){
+        if(regex_match(op, skipOperations)) {
+            if(op == string("byte")) {
                 string objectCode = byteHandler(operand);
                 opTab[j].setLongObjectCode(objectCode);
                 cout << opTab[j].toString() <<endl;
                 j++;
                 continue;
-            } else if(op == string("word")){
+            } else if(op == string("word")) {
                 try {
                     int objectCode = stoi(operand);
-                    if(objectCode > 4095){
+                    if(objectCode > 4095) {
                         opTab[j].setPass2ErrMsg("operand overflow");
                         opTab[j].setOpCode(0);
-                continue;
+                        j++;
+                        continue;
                     } else {
                         opTab[j].setOpCode(objectCode);
                     }
-                    //cout << opTab[j].toString() <<endl;
+                    cout << opTab[j].toString() <<endl;
                     j++;
                     continue;
-                } catch(invalid_argument arg){
+                } catch(invalid_argument arg) {
                     opTab[j].setPass2ErrMsg("illegal operand");
                     opTab[j].setOpCode(0);
-                //continue;
-                    //cout << opTab[j].toString() <<endl;
+                    //continue;
+                    cout << opTab[j].toString() <<endl;
                     j++;
                     continue;
                 }
@@ -115,14 +116,15 @@ vector<Instruction> Pass2::execute() {
                 i = true;
                 operand = operand.substr(1, operand.size());
                 //operandHex = getNumericOperand(operand);
-                try{
+                try {
                     operandHex = stoi(operand);
-                    if((operandHex < -2048 || operandHex > 4095) && !e){
+                    if((operandHex < -2048 || operandHex > 4095) && !e) {
                         opTab[j].setPass2ErrMsg("address exceeds size try to use extended format");
                         opTab[j].setOpCode(0);
-                continue;
-                    } else if (operandHex < 0){
-                        if(e){
+                        j++;
+                        continue;
+                    } else if (operandHex < 0) {
+                        if(e) {
                             operandHex = twoscomp(operandHex, 20);
                         } else {
                             operandHex = twoscomp(operandHex, 12);
@@ -135,7 +137,8 @@ vector<Instruction> Pass2::execute() {
                     if(iter == symTab.end()) {
                         opTab[j].setPass2ErrMsg("using undefined symbol");
                         opTab[j].setOpCode(0);
-                continue;
+                        j++;
+                        continue;
                     } else {
                         operandHex = iter->second.address;
                         disp = displacement(locctr, operandHex, base, e, b, p);
@@ -144,7 +147,8 @@ vector<Instruction> Pass2::execute() {
                 if(operandHex == 16777215) {
                     opTab[j].setPass2ErrMsg("symbol not found or address exceeded size");
                     opTab[j].setOpCode(0);
-                continue;
+                    j++;
+                    continue;
                 }
                 //disp = displacement(locctr, operandHex, base, e, b, p);
             } else if (operand[0] == '@') {
@@ -154,7 +158,8 @@ vector<Instruction> Pass2::execute() {
                 if(operandHex == 16777215) {
                     opTab[j].setPass2ErrMsg("symbol not found or address exceeded size");
                     opTab[j].setOpCode(0);
-                continue;
+                    j++
+                    continue;
                 }
                 disp = displacement(locctr, operandHex, base, e, b, p);
             } else if (operand[0] == '=') {
@@ -163,7 +168,8 @@ vector<Instruction> Pass2::execute() {
                 if(operandHex == 16777215) {
                     opTab[j].setPass2ErrMsg("literal not found or address exceeded size");
                     opTab[j].setOpCode(0);
-                continue;
+                    j++;
+                    continue;
                 }
                 //disp = displacement(locctr, operandHex, base, e, b, p);
                 disp = operandHex;
@@ -181,15 +187,16 @@ vector<Instruction> Pass2::execute() {
                 if(operandHex == 16777215) {
                     opTab[j].setPass2ErrMsg("symbol not found or address exceeded size");
                     opTab[j].setOpCode(0);
-                continue;
+                    j++;
+                    continue;
                 }
                 disp = displacement(locctr, operandHex, base, e, b, p);
-            } else if (isExpression(operand)){
+            } else if (isExpression(operand)) {
                 //cout << "expression" <<endl;
-                if(operand[0] == '#'){
+                if(operand[0] == '#') {
                     i = true;
                     n = false;
-                } else if (operand[0] == '@'){
+                } else if (operand[0] == '@') {
                     n = true;
                     i = false;
                 } else {
@@ -198,22 +205,23 @@ vector<Instruction> Pass2::execute() {
                 }
                 operandHex = getExpressionValue(operand);
                 //cout << hex <<operandHex <<endl;
-                if(operandHex == 16777215 || operandHex < 0){
+                if(operandHex == 16777215 || operandHex < 0) {
                     opTab[j].setPass2ErrMsg("invalid expression or negative value address");
                     opTab[j].setOpCode(0);
-                continue;
+                    j++;
+                    continue;
                 }
                 disp = displacement(locctr, operandHex, base, e, b, p);
             } else {
-                if(opHex == 0x90 || opHex == 0xA0 || opHex == 0x9C || opHex == 0x98 || opHex == 0xAC || opHex == 0x94){
+                if(opHex == 0x90 || opHex == 0xA0 || opHex == 0x9C || opHex == 0x98 || opHex == 0xAC || opHex == 0x94) {
                     string r1;
                     string r2;
                     parseReg(operand, r1, r2);
                     disp = regValue(r1) << 4;
                     disp = disp | regValue(r2);
-                } else if (opHex == 0xB4 || opHex == 0xB8){
+                } else if (opHex == 0xB4 || opHex == 0xB8) {
                     disp = regValue(operand) << 4;
-                } else if (opHex == 0xA4 || opHex == 0xA8){
+                } else if (opHex == 0xA4 || opHex == 0xA8) {
                     string r1;
                     string n;
                     parseReg(operand, r1, n);
@@ -227,20 +235,23 @@ vector<Instruction> Pass2::execute() {
                     if(operandHex == 16777215) {
                         opTab[j].setPass2ErrMsg("symbol not found or address exceeded size");
                         opTab[j].setOpCode(0);
-                continue;
+                        j++;
+                        continue;
                     }
                     disp = displacement(locctr, operandHex, base, e, b, p);
                 }
 
             }
 
-            if(b && disp == 16777215){
+            if(b && disp == 16777215) {
                 opTab[j].setPass2ErrMsg("base relative while base is not set");
                 opTab[j].setOpCode(0);
+                j++;
                 continue;
-            } else if (disp == 16777215){
+            } else if (disp == 16777215) {
                 opTab[j].setPass2ErrMsg("address overflow");
                 opTab[j].setOpCode(0);
+                j++;
                 continue;
             }
         } else {
@@ -277,40 +288,40 @@ string getIndexedSymbol(string indexed) {
     }
 }
 
-void parseReg(string operand, string &r1, string &r2){
+void parseReg(string operand, string &r1, string &r2) {
     regex r("([AXLBSTFaxlbstf]) *, *([AXLBSTFaxlbstf])");
-    if(regex_match(operand, r)){
+    if(regex_match(operand, r)) {
         smatch m;
-        if(regex_search(operand, m, r)){
+        if(regex_search(operand, m, r)) {
             r1 = m.str(1);
             r2 = m.str(2);
         }
     }
 }
 
-int regValue(string reg){
-    if(reg == string("a") || reg == string("A")){
+int regValue(string reg) {
+    if(reg == string("a") || reg == string("A")) {
         return 0;
-    } else if (reg == string("x") || reg == string("X")){
+    } else if (reg == string("x") || reg == string("X")) {
         return 1;
-    } else if (reg == string("l") || reg == string("L")){
+    } else if (reg == string("l") || reg == string("L")) {
         return 2;
-    } else if (reg == string("b") || reg == string("B")){
+    } else if (reg == string("b") || reg == string("B")) {
         return 3;
-    } else if (reg == string("s") || reg == string("S")){
+    } else if (reg == string("s") || reg == string("S")) {
         return 4;
-    } else if (reg == string("t") || reg == string("T")){
+    } else if (reg == string("t") || reg == string("T")) {
         return 5;
-    } else if (reg == string("f") || reg == string("F")){
+    } else if (reg == string("f") || reg == string("F")) {
         return 6;
     }
 }
-unordered_map<string, pair<int ,string>> Pass2::getLitTab() {
+unordered_map<string, pair<int,string>> Pass2::getLitTab() {
     return litTab;
 }
-void Pass2::setLitTab(unordered_map<string, pair<int ,string>> litTab) {
+void Pass2::setLitTab(unordered_map<string, pair<int,string>> litTab) {
     this->litTab = litTab;
-    unordered_map<string, pair<int ,string>>::const_iterator iter = litTab.begin();
+    unordered_map<string, pair<int,string>>::const_iterator iter = litTab.begin();
     /*while(iter != litTab.end()){
         cout << "                                    >>>>>>"<<iter->first << "    " << iter->second.first << "     " << iter->second.second << endl;
         iter++;
@@ -352,7 +363,7 @@ int Pass2::getLiteralValue(string operand) {
             string s = m.str(2);
             if(type == string("x") || type == string("X")) {
                 unordered_map<string, pair<int, string>>::const_iterator iter = litTab.find(s);
-                if(iter == litTab.end()){
+                if(iter == litTab.end()) {
                     return 16777215;
                 } else {
                     return iter->second.first;
@@ -366,7 +377,7 @@ int Pass2::getLiteralValue(string operand) {
                 string hex = hexa_format_of_literal(s);
                 transform(hex.begin(), hex.end(), hex.begin(), ::toupper);
                 unordered_map<string, pair<int, string>>::const_iterator iter = litTab.find(hex);
-                if(iter == litTab.end()){
+                if(iter == litTab.end()) {
                     cout << "ml2aaaash" << endl;
                     return 16777215;
                 } else {
@@ -404,16 +415,16 @@ string hexa_format_of_literal(string str) {
     return hexa_form;
 }
 
-string byteHandler(string operand){
+string byteHandler(string operand) {
     regex r("([Cc]|[Xx])\'(.+)\'");
-    if(regex_match(operand, r)){
+    if(regex_match(operand, r)) {
         smatch m;
-        if(regex_search(operand, m, r)){
+        if(regex_search(operand, m, r)) {
             string type = m.str(1);
             string value = m.str(2);
-            if(type == string("x") || type == string("X")){
+            if(type == string("x") || type == string("X")) {
                 return value;
-            } else if (type == string("c") || type == string("C")){
+            } else if (type == string("c") || type == string("C")) {
                 return hexa_format_of_literal(value);
             }
         }
@@ -421,31 +432,31 @@ string byteHandler(string operand){
     return string("");
 }
 
-bool isExpression(string operand){
+bool isExpression(string operand) {
     regex r("((#|@)?([a-zA-Z0-9]+))(\\+|\\-|\\*|\\/)([a-zA-Z0-9]+)");
     //cout << regex_match(operand, r);
     return regex_match(operand, r);
 }
 
-int Pass2::getExpressionValue(string operand){
+int Pass2::getExpressionValue(string operand) {
     regex r("([a-zA-Z0-9]+)(\\+|\\-|\\*|\\/)([a-zA-Z0-9]+)");
     smatch m;
-    if(regex_search(operand, m, r)){
+    if(regex_search(operand, m, r)) {
         string sym1 = m.str(1);
         string op = m.str(2);
         string sym2 = m.str(3);
         int v1 = getNumericOperand(sym1);
         int v2 = getNumericOperand(sym2);
-        if(v1 == 16777215 || v2 == 16777215){
+        if(v1 == 16777215 || v2 == 16777215) {
             return 16777215;
         }
-        if(op == string("+")){
+        if(op == string("+")) {
             return v1+v2;
-        } else if(op == string("-")){
+        } else if(op == string("-")) {
             return v1 - v2;
-        } else if(op == string("*")){
+        } else if(op == string("*")) {
             return v1*v2;
-        } else if(op == string("/")){
+        } else if(op == string("/")) {
             return v1/v2;
         }
     }
